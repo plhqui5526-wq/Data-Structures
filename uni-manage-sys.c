a9d72a8427e21b886170213d4f9b27347283150b
Le-Hoang-Qui Phan <plhqui5526@gmail.com>
2026-09-16 07:59:24 +0700

    created and initialized university management system project

---
+++ uni-manage-sys.c
@@ -0,0 +1,14 @@
+typedef struct {
+    char id[10];
+    char name[50];
+    float gpa;
+    int credits;
+} Student;
+
+Student students[] = {
+    {"S001", "Nguyen An", 3.20, 65},
+    {"S002", "Tran Binh", 3.75, 82},
+    {"S003", "Le Chi", 2.95, 58},
+    {"S004", "Pham Dung", 3.90, 91},
+    {"S005", "Hoang Giang", 3.45, 76}
+};
